48553aeee5ac76c92a3f420fb9e42cc91f676834
Peeke Kuepers <pkuepers@kaliber.net>
2022-11-02 22:18:20 +0100

    Add sticker to App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
-export default function App() {
-  return <div>APP</div>
+import { pseudoRandom } from '@kaliber/math'
+import sticker from '/images/stickers/cactus.svg'
+import styles from './App.css'
+
+export default function App({ seed }) {
+  const x = pseudoRandom(`${seed}x`)
+  const y = pseudoRandom(`${seed}y`)
+
+  return (
+    <div className={styles.app}>
+      <img className={styles.sticker} src={sticker} style={{ '--x': x, '--y': y }} alt='' />
+    </div>
+  )
 }
