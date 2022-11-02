2334631c5e4e029d0e8b177684c1f82ae94b6299
Peeke Kuepers <pkuepers@kaliber.net>
2022-11-02 21:14:56 +0100

    Run scaffold command

---
+++ src/index.html.js
@@ -0,0 +1,39 @@
+import '/reset.css'
+import '/index.css'
+import stylesheet from '@kaliber/build/lib/stylesheet'
+import javascript from '@kaliber/build/lib/javascript'
+import polyfill from '@kaliber/build/lib/polyfill'
+import rollbar from '@kaliber/build/lib/rollbar'
+import config from '@kaliber/config'
+import App from '/App?universal'
+
+Index.routes = {
+  match(location) {
+    const path = location.pathname
+    if (path === '/') return { status: 200 }
+    else return { status: 404, data: { notFound: true } }
+  }
+}
+
+export default function Index({ location, data }) {
+  return (
+    <html lang='nl'>
+      <head>
+        <meta charSet='utf-8' />
+        <title>@kaliber/build</title>
+        <meta name='description' content='' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        {stylesheet}
+        {rollbar({ accessToken: config.rollbar.post_client_item })}
+        {polyfill(['default', 'es2015', 'es2016', 'es2017', 'es2018', 'es2019'])}
+        {javascript}
+      </head>
+      <body>
+        {data && data.notFound
+          ? 'Not found'
+          : <App config={config.client} />
+        }
+      </body>
+    </html>
+  )
+}
