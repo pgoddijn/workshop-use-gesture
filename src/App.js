38ea8c4f5fac346dc7605db0d4c2b9d3f75fbb66
Peeke Kuepers <pkuepers@kaliber.net>
2022-11-03 16:31:46 +0100

    Fix spring api

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App({ seed }) {
   const x = pseudoRandom(`${seed}x`)
   const y = pseudoRandom(`${seed}y`)
 
-  const [spring, api] = useSpring({ x: 0, y: 0 })
+  const [spring, api] = useSpring(() => ({ x: 0, y: 0 }))
 
   return (
     <div className={styles.app} style={{ '--x': x, '--y': y }}>
