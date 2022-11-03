9f0e7cc2019b1b9b19b3bf599d35823b32f6cc21
Peeke Kuepers <pkuepers@kaliber.net>
2022-11-03 21:15:46 +0100

    Add avocado with separate pit image

--- src/App.universal.js
+++ src/App.universal.js
@@ -1,3 +1,12 @@
+import pit from '/images/avocados/pit.svg'
+import styles from './App.css'
+
 export default function App() {
-  return <div>APP</div>
+  return (
+    <div className={styles.component}>
+      <div className={styles.avocado}>
+        <img className={styles.pit} src={pit} alt='' />
+      </div>
+    </div>
+  )
 }
