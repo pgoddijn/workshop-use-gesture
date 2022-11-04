cd721405766ef4599c357f67ed2a1001b9cef0f4
Peter Goddijn <peter.goddijn@nxvalley.com>
2022-11-04 15:55:06 +0100

    You know, a commit

--- src/App.universal.js
+++ src/App.universal.js
@@ -1,12 +1,29 @@
 import pit from '/images/avocados/pit.svg'
 import styles from './App.css'
+import { useDrag } from '@use-gesture/react'
+import { useSpring } from '@react-spring/web'
 
 export default function App() {
+
+  const [spring, api] = useSpring(() => ({ x: 0, y: 0 }))
+  const bind = useDrag(HandleDrag, { /* options */})
+
   return (
     <div className={styles.component}>
       <div className={styles.avocado}>
-        <img className={styles.pit} src={pit} alt='' />
+        <img className={styles.pit}
+          src={pit}
+          alt=''
+          style={spring}
+          draggable="false"
+          {...bind()}
+        />
       </div>
     </div>
   )
+  function HandleDrag(state) {
+    const [ x,y ] = state.offset
+    api.start ( { x,y } )
+    console.log(state)
+  }
 }
