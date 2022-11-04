b11cbb5fcf6e794ef9ce65039ae20938c5a09500
Peter Goddijn <peter.goddijn@nxvalley.com>
2022-11-04 15:20:58 +0100

    You know, a commit

--- src/App.universal.js
+++ src/App.universal.js
@@ -1,10 +1,12 @@
 import { useSpring, animated } from '@react-spring/web'
 import { pseudoRandom } from '@kaliber/math'
 import styles from './App.css'
+import { useDrag } from '@use-gesture/react'
 
 export default function App({ sticker, seed }) {
   const x = pseudoRandom(`${seed}x`)
   const y = pseudoRandom(`${seed}y`)
+  const bind = useDrag(HandleDrag, { /* options */})
 
   const [spring, api] = useSpring(() => ({ x: 0, y: 0 }))
 
@@ -15,7 +17,15 @@ export default function App({ sticker, seed }) {
         src={sticker}
         alt=''
         style={spring}
+        draggable="false"
+        // useDrag="false"
+        {...bind()}
       />
     </div>
   )
+  function HandleDrag(state) {
+    const [ x,y ] = state.offset
+    api.start ( { x,y } )
+    console.log(state)
+  }
 }
