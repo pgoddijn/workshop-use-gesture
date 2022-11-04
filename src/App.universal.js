b009570ab26b32981e02482cd9f371e900043518
Peeke Kuepers <pkuepers@kaliber.net>
2022-11-04 13:24:10 +0100

    Add basic Rive instance

--- src/App.universal.js
+++ src/App.universal.js
@@ -1,3 +1,22 @@
+import { useRive, useStateMachineInput } from '@rive-app/react-canvas'
+import { lerp } from '@kaliber/math'
+import bike from '/rive/bike.riv'
+import styles from './App.css'
+
 export default function App() {
-  return <div>APP</div>
+  const { rive, RiveComponent } = useRive({
+    src: bike,
+    autoplay: true,
+    stateMachines: 'State Machine 1'
+  })
+
+  const input = useStateMachineInput(rive, 'State Machine 1', 'numRotation', 0)
+
+  return (
+    <div className={styles.component}>
+      <RiveComponent onClick={() => {
+        input.value = lerp({ start: -100, end: 100, input: Math.random() })
+      }} />
+    </div>
+  )
 }
